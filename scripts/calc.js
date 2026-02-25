15fac06836a057cbf557f089bb611d5d094b9734
Piotr Makuch <piotr.makuch19@gmail.com>
2026-02-25 12:48:59 +0100

    refactor: simplify calculator state managment and event handling - replace getter functions with input event listeners - refactor getGenderValue() to getGender() - remove redundant DOM value reads - update BMR and TDEE calculation logic - simplify form submit handler

--- scripts/calc.js
+++ scripts/calc.js
@@ -7,128 +7,120 @@ const weightInput = document.getElementById("metric__weight");
 const activityLevelDropdown = document.getElementById("activity_lvl");
 const calculateBtn = document.getElementById("calculate_btn");
 const calculateForm = document.getElementById("calc_form");
-const genderResult = document.getElementById("gender_result");
+const messegeContainer = document.getElementById("messege_cnt");
 
 
 
-function changeBackground() {
-  ageInput.style.backgroundColor = "red";
-}
+let genderValueFemale;
+genderFemale.addEventListener("change", (e) => {
 
-let genderValue; 
-function getGenderValue() {
-  if (genderFemale.checked) {
-   return genderValue = genderFemale.value;
-    
-  } else if (genderMale.checked) {
-    return genderValue = genderMale.value;
+  genderValueFemale = Number(e.target.value);
+  genderValueMale = "";
+  getGender();
+  
+});
+
+let genderValueMale;
+genderMale.addEventListener("change", (e) => {
+  
+  genderValueMale = Number(e.target.value);
+  genderValueFemale = "";
+  getGender();
+
+});
+
+
+
+let genderValue;
+function getGender() {
+
+  if (genderValueFemale == 1) {
+    return genderValue = Number(genderValueFemale);
+  } else if (genderValueMale == 2) {
+    return genderValue = Number(genderValueMale);
   } else {
-    //add alert
-    console.log("no gender");
-    return;
+    messegeContainer.innerHTML = "choose a gender";
+    return genderValue = 0;
   }
 }
 
-let ageInputValue;
-function getAgeInput() {
-  return ageInputValue = ageInput.value;
-  
-}
 
 
-let heightInputValue;
-function getHeightValue() {
-  return heightInputValue = heightInput.value;
-}
 
-let weightInputValue;
-function getWeightValue() {
- return weightInputValue = weightInput.value;
-}
+let ageValue = 0;
+ageInput.addEventListener("input", (e) => {
+  ageValue = Number(e.target.value);
+});
+
+let heightValue = 0;
+heightInput.addEventListener("input", (e) => {
+  heightValue = Number(e.target.value);
+});
+
+let weightValue = 0;
+weightInput.addEventListener("input", (e) => {
+  weightValue = Number(e.target.value);
+});
 
 let activitySelected;
-
-function activitySelectedToNumber() {
-  const activitySelectedNumber = Number(activitySelected);
-  return activitySelectedNumber;
-}
-
-function getActivitySelected() {
-  activitySelected = activityLevelDropdown.value;
-  return activitySelectedToNumber();
-
-  }
+activityLevelDropdown.addEventListener("change", (e) => {
+  activitySelected = Number(e.target.value);
+});
 
 
-const printScore = function() {
-  genderResult.innerHTML = `
-  <span>Your Result: ${genderValue} </span><br>
-  <span>your age: ${ageInputValue} </span><br>
-  <span>Choosen activity rate: ${activitySelected}</span><br>
-  <span>Your TDEE: ${calculateTdee()};</span>
-  
 
-  `
-};
 
 const extraCaloriesForWomen = -161;
 const extraCaloriesForMen = 5;
 
+
+
 let bmrRate;
 function calculateBmr(gender) {
   
-  if (gender == 1) {
-    bmrRate = 10 * weightInputValue + 6.25 * heightInputValue - 5 * ageInputValue + extraCaloriesForMen;
-    genderResult.style.backgroundColor = "red";
+  if (gender == 2) {
+    bmrRate = 10 * weightValue + 6.25 * heightValue - 5 * ageValue + extraCaloriesForMen;
     console.log(bmrRate, "Male gender");
     return bmrRate; 
-  } else if (gender == 2 ) {
-    bmrRate = 10 * weightInputValue + 6.25 * heightInputValue - 5 * ageInputValue + extraCaloriesForWomen;
+  } else if (gender == 1 ) {
+    bmrRate = 10 * weightValue + 6.25 * heightValue - 5 * ageValue + extraCaloriesForWomen;
     console.log(bmrRate, "Female gender");
     return bmrRate;
   } else {
-    genderResult.innerHTML = "Choose a gender";
+    messegeContainer.innerHTML = "Choose a gender";
     console.log("choose gen"); 
     return;
   }
-
+  
 }
 
 
+
+
 function calculateTdee() {
-  let Tdee = Math.floor(calculateBmr(getGenderValue()) * getActivitySelected());
-  console.log(Tdee);
+  let Tdee = Math.floor(bmrRate * activitySelected);
   return Tdee;
 }
 
 
+
+const printScore = function() {
+  messegeContainer.innerHTML = `
+
+  <span>Your TDEE: ${calculateTdee()};</span>
+  `
+};
+
+
+
 calculateForm.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  getGenderValue();
-  console.log(getGenderValue());
-
-  getAgeInput();
-  console.log(getAgeInput());
-
-  getHeightValue();
-  console.log(getHeightValue());
-
-  getWeightValue();
-  console.log(getWeightValue());
-
-  getActivitySelected();
-
-  calculateBmr(getGenderValue());
-
-  calculateTdee();
- // console.log(calculateTdee());
   
+  calculateBmr(genderValue);
+  const tdee = calculateTdee();
   printScore();
-  changeBackground();
-  
-  
-})
+
+});
 
 
 
