81ba360c1c3339d7dfd189992675e7e003a4687a
Piotr Makuch <piotr.makuch19@gmail.com>
2026-03-04 11:39:33 +0100

    feat: add validation to empty inputs - add a validation logic if inputs are empty - add required alert container in HTML

--- scripts/calc.js
+++ scripts/calc.js
@@ -4,9 +4,11 @@ const heightInput = document.getElementById("metric__height");
 const weightInput = document.getElementById("metric__weight");
 const activityLevelDropdown = document.getElementById("activity_lvl");
 const calculateForm = document.getElementById("calc_form");
+const genderAlertContainer = document.getElementById("gender_alert_cnt");
 const ageAlertContainer = document.getElementById("age_alert_cnt");
 const heightAlertContainer = document.getElementById("height_alert_cnt");
 const weightAlertContainer = document.getElementById("weight_alert_cnt");
+const activityAlertContainer = document.getElementById("activity_alert_cnt");
 const messegeContainer = document.getElementById("messege_cnt");
 
 
@@ -18,24 +20,24 @@ const weightRange = { min: 20, max: 250, alertLokalization: weightAlertContainer
 
 
 
+   
+   
 function isValid(range, inputType) {
   
   inputType.addEventListener("input", () => {
     if (inputType.value === "") {
-      console.log("empty");
       range.alertLokalization.innerHTML = "";
+      return falseValue;
     }
     else if (inputType.value < range.min || inputType.value > range.max) {
       range.alertLokalization.innerHTML = 
       `<span>Choose value between ${range.min} and ${range.max}</span>
       `;
     } else {
-      //maybe check marK?
       range.alertLokalization.innerHTML = "";
-      console.log("ok")
     }
   })
-
+  
 }
 
 isValid(ageRange, ageInput);
@@ -65,32 +67,51 @@ function printScore(tdee) {
   `
   
 };
-
+    
 
 calculateForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+      e.preventDefault();
   
   const selectedGender = document.querySelector('input[name="gender"]:checked');
-
+      
   if (!selectedGender) {
-    messegeContainer.innerHTML = "Choose a gender";
-    return;
+    genderAlertContainer.innerHTML = "Wybierz płeć";
+  } else genderAlertContainer.innerHTML = "";
+  
+  if (ageInput.value === "") {
+    ageAlertContainer.innerHTML = "Podaj wiek";
+  } else ageAlertContainer.innerHTML = "";
+  
+  if (heightInput.value === "") {
+    heightAlertContainer.innerHTML = "Podaj wzrost";
   }
+  
+  if (weightInput.value === "") {
+    weightAlertContainer.innerHTML = "Podaj wagę";
+  }
+  
+  const activity = Number(activityLevelDropdown.value);
+  
+  if (activity == "0") {
+    activityAlertContainer.innerHTML = "Wybierz poziom aktywności";
+    } else  {
+      activityAlertContainer.innerHTML = "";
+      console.log("activity has been choosen")
+    }
+  
+    
   
   const gender = Number(selectedGender.value);
   const age = Number(ageInput.value);
   const weight = Number(weightInput.value);
   const height = Number(heightInput.value);
-  const activity = Number(activityLevelDropdown.value);
-  
 
- const extraCalories = getExtraCalories(gender);
- const bmr = calculateBmr(weight, height, age, extraCalories);
- const tdee = calculateTdee(bmr, activity);
- 
+  const extraCalories = getExtraCalories(gender);
+  const bmr = calculateBmr(weight, height, age, extraCalories);
+  const tdee = calculateTdee(bmr, activity);
+   
  printScore(tdee);
 
- //alert that input form cannot be empty
 
 });
 
