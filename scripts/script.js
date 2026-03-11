a97e68117fe44bc9d58c89f104ced3d1e17eada6
Piotr Makuch <piotr.makuch19@gmail.com>
2026-03-11 12:37:15 +0100

    feat: add hamburger menu toggle functionality
    - update HTML structure for mobile navigation
    - add JS toggle logic for opening and closing the menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
-console.log("siema Piotrek");
+const body = document.querySelector("body");
+const navbarCollapseContainer = document.getElementById("nav-collapse-cnt");
+const hamburgerContainer = document.getElementById("navbar-hamburger-cnt");
+const closeHamburgerContainer = document.getElementById("navbar-hamburger-close");
+const hamburgerElement = document.getElementById("navbar-hamburger-el");
+
+function activateToggleCollapseContainer() {
+  navbarCollapseContainer.classList.toggle("visible");
+  changeSymbolToClose();
+}
+
+function collapseMenu() {
+  navbarCollapseContainer.classList.remove("visible");
+  changeSymbolToMenu();
+}
+
+function changeSymbolToClose() {
+  hamburgerElement.innerText = "close";
+}
+
+function changeSymbolToMenu() {
+  hamburgerElement.innerText = "menu";
+}
+
+
+
+hamburgerContainer.addEventListener("click", activateToggleCollapseContainer);
+closeHamburgerContainer.addEventListener("click", collapseMenu);
+
+
+
