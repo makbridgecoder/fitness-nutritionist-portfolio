44ab9f758c3f840c3791d8c324af2d20c55c12ef
makbridgecoder <piotr.makuch19@gmail.com>
2026-09-04 10:35:45 +0200

    fix: improve mobile menu (#9)
    - improve mobile navigation on the about page
    - add additional li "Strona główna" to mobile menu

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,4 @@ const purchasedProductsArray = getItemFromLocalStorage();
 renderBasketCount(purchasedProductsArray);
 
 
-//DONE: basket counter works  :)  
-//NEXT: remove calculatetotalprice from shop, commit
-//BLOCKED BY: what next?
+
